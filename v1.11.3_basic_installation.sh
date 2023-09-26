25ab86cbdcb61baa8afa894be40bc04f3ccd3d39
Nihar Mishra <nihar.mishra@ibm.com>
2023-09-27 00:24:04 +0530

    folder

--- v1.11.3_basic_installation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.11.3/eventing-crds.yaml
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.11.3/eventing-core.yaml
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.11.3/in-memory-channel.yaml
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.11.3/mt-channel-broker.yaml
-
